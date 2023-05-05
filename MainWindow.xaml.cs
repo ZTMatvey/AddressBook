be3970ffd3853a007873465a8fb83d56d6ee8f7b
zenot <zenot@DESKTOP-N3F9INH>
2023-05-05 14:39:04 +0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AddressBook
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly BitmapImage _minimize;
+        private readonly BitmapImage _maximize;
+        private bool _isMinimized;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _minimize = new BitmapImage();
+            _minimize.BeginInit();
+            _minimize.UriSource = new Uri("pack://application:,,,/Images/close_fullscreen.png");
+            _minimize.EndInit();
+            _maximize = new BitmapImage();
+            _maximize.BeginInit();
+            _maximize.UriSource = new Uri(@"pack://application:,,,/Images/open_in_full.png");
+            _maximize.EndInit();
+            Minimize();
+        }
+
+        private void CloseApp_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                DragMove();
+            }
+        }
+
+        private void Minimize()
+        {
+            _isMinimized = true;
+            WindowState = WindowState.Normal;
+            MaxMinApp.Background = new ImageBrush(_maximize);
+        }
+
+
+        private void Maximize()
+        {
+            _isMinimized = false;
+            WindowState = WindowState.Maximized;
+            MaxMinApp.Background = new ImageBrush(_minimize);
+        }
+
+        private void MaxMinApp_Click(object sender, RoutedEventArgs e)
+        {
+            if (_isMinimized)
+            {
+                Maximize();
+            }
+            else
+            {
+                Minimize();
+            }
+        }
+    }
+}
---
+++ ViewModel/NavigationVM.cs
@@ -0,0 +1,42 @@
+﻿using HelpDef.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AddressBook.ViewModel
+{
+    internal sealed class NavigationVM : ViewModelBase
+    {
+        private object _currentView;
+        public object CurrentView
+        {
+            get
+            {
+                return _currentView;
+            }
+            private set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand HomeCommand { get; set; }
+
+        public ICommand BookCommand { get; set; }
+
+        public NavigationVM()
+        {
+            Home(new object());
+            HomeCommand = new RelayCommand(execute: Home);
+            BookCommand = new RelayCommand(execute: Book);
+        }
+
+        private void Home(object obj) => CurrentView = new HomeVM();
+
+        private void Book(object obj) => CurrentView = new BookVM();
+    }
+}
